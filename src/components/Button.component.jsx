785f29fa0d1408cfd404a88a346b7115111bf79c
Zenith Hanif <zenithpkhanif@gmail.com>
2024-04-21 19:30:14 +0500

    Added. Special Offer section. Modified button component for button attributes

--- src/components/Button.component.jsx
+++ src/components/Button.component.jsx
@@ -1,6 +1,9 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 
+                        border font-montserrat text-lg leading-none 
+                        ${
+                          backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : "bg-coral-red text-white border-coral-red"} rounded-full w-full"}`}>
         {label}
         {
           iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />
@@ -9,4 +12,4 @@ const Button = ({ label, iconURL }) => {
   )
 }
 
-export default Button
+export default Button;
