50daf523bd62788645a493cf87e34a779900c5ca
Zenith Hanif <zenithpkhanif@gmail.com>
2024-04-21 20:11:00 +0500

    Added Subscribe section

--- src/components/Button.component.jsx
+++ src/components/Button.component.jsx
@@ -1,9 +1,10 @@
-const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 
                         border font-montserrat text-lg leading-none 
                         ${
-                          backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : "bg-coral-red text-white border-coral-red"} rounded-full w-full"}`}>
+                          backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : "bg-coral-red text-white border-coral-red"} 
+                        rounded-full ${fullWidth && 'w-full'}"}`}>
         {label}
         {
           iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />
--- src/sections/Subscribe.section.jsx
+++ src/sections/Subscribe.section.jsx
@@ -1,6 +1,19 @@
+import Button from "../components/Button.component"
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section id="contact-us" className="max-container flex flex-col justify-between items-center max-lg:flex-col gap-10">
+        <h3 className="text-4xl leading-[68px] font-palanquin font-bold">Sign Up for
+          <span className="text-coral-red"> Updates</span> & Newsletter
+        </h3>
+        <div className="lg:max-w-[70%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+          <input type="text" placeholder="subscribe@nike.com"
+          className="input"/>
+          <div className="flex mx-sm:justify-end items-center mx-sm:w-full">
+            <Button label="Sign Up" fullWidth/>
+          </div>
+        </div>
+    </section>
   )
 }
 
